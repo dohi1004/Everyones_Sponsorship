a7b61aa6d7395a3625714de3f6ac0d9a6a8c08dc
sperospera1225@gmail.com <sperospera1225@gmail.com>
2021-11-12 22:14:12 +0900

    11.12

--- app/src/main/java/com/example/everyones_sponsorship/SignAdvertiserActivity.kt
+++ app/src/main/java/com/example/everyones_sponsorship/SignAdvertiserActivity.kt
@@ -3,7 +3,11 @@ package com.example.everyones_sponsorship
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.everyones_sponsorship.databinding.ActivitySignAdvertiserBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignAdvertiserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +18,63 @@ class SignAdvertiserActivity : AppCompatActivity() {
         // db에 id, pwd 저장하는 로직 구현
 
         binding.submit.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            when{
+                TextUtils.isEmpty(binding.usernameAdvertiser.text.toString().trim{it<=' '}) -> {
+                    Toast.makeText(
+                        this@SignAdvertiserActivity,
+                        "Please enter username.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(binding.passwordAdvertiser.text.toString().trim{it<=' '}) -> {
+                    Toast.makeText(
+                        this@SignAdvertiserActivity,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(binding.businessId.text.toString().trim{it<=' '}) -> {
+                    Toast.makeText(
+                        this@SignAdvertiserActivity,
+                        "Please enter InstagramID.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = binding.usernameAdvertiser.text.toString().trim{it <= ' '}
+                    val password: String = binding.passwordAdvertiser.text.toString().trim{it <= ' '}
+                    val snsid: String = binding.businessId.text.toString().trim{it <= ' '}
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                Toast.makeText(
+                                    this@SignAdvertiserActivity,
+                                    "You are registered successfully.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this@SignAdvertiserActivity, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", firebaseUser.uid)
+                                intent.putExtra("email_id", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@SignAdvertiserActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+
+                }
+            }
+
         }
     }
 }

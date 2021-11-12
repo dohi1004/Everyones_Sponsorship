d8f698e5ce01b5fcda9f18849040da7c654dabfe
dohui <dohi100430@gmail.com>
2021-11-12 20:07:53 +0900

    splash activity done

--- app/src/main/java/com/example/everyones_sponsorship/SplashActivity.kt
+++ app/src/main/java/com/example/everyones_sponsorship/SplashActivity.kt
@@ -2,19 +2,28 @@ package com.example.everyones_sponsorship
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        try{
-            Thread.sleep(2000);
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        }catch(e: Exception){
-            return;
-        }
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 
 }

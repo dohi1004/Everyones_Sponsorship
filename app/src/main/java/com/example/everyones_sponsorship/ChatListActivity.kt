1a2912f75dcea05aeab91cfed679053cb46bdb86
sperospera1225@gmail.com <sperospera1225@gmail.com>
2021-11-20 16:41:56 +0900

    minseon

--- app/src/main/java/com/example/everyones_sponsorship/ChatListActivity.kt
+++ app/src/main/java/com/example/everyones_sponsorship/ChatListActivity.kt
@@ -2,20 +2,131 @@ package com.example.everyones_sponsorship
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.everyones_sponsorship.databinding.ActivityChatlistBinding
+import com.example.everyones_sponsorship.fragment.ChatFragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 
 class ChatListActivity : AppCompatActivity() {
     private val fireDatabase = FirebaseDatabase.getInstance().reference
+
+    inner class ChatModel (val users: HashMap<String, Boolean> = HashMap(),
+                           val comments : HashMap<String, Comment> = HashMap()){
+        inner class Comment(val uid: String? = null, val message: String? = null, val time: String? = null)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityChatlistBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent = Intent(this, ChattingActivity::class.java)
-        intent.putExtra("destinationUid", "InfluencerName")
-        startActivity(intent)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.chatfragment_recyclerview)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL ,false)
+        recyclerView.adapter = RecyclerViewAdapter()
+//        val intent = Intent(this, ChattingActivity::class.java)
+//        intent.putExtra("destinationUid", "InfluencerName")
+//        startActivity(intent)
     }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.CustomViewHolder>() {
+
+        private val chatModel = ArrayList<ChatFragment.ChatModel>()
+        private var uid : String? = null
+        private val destinationUsers : ArrayList<String> = arrayListOf()
+
+        init {
+            uid = Firebase.auth.currentUser?.uid.toString()
+            println(uid)
+
+            fireDatabase.child("chatrooms").orderByChild("users/$uid").equalTo(true).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    chatModel.clear()
+                    for(data in snapshot.children){
+                        chatModel.add(data.getValue<ChatFragment.ChatModel>()!!)
+                        println(data)
+                    }
+                    notifyDataSetChanged()
+                }
+
+            })
+
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
+
+            return CustomViewHolder(LayoutInflater.from(this@ChatListActivity).inflate(R.layout.item_chat, parent, false))
+        }
+
+        inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val imageView: ImageView = itemView.findViewById(R.id.chat_item_imageview)
+            val textView_title : TextView = itemView.findViewById(R.id.chat_textview_title)
+            val textView_lastMessage : TextView = itemView.findViewById(R.id.chat_item_textview_lastmessage)
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+            var destinationUid: String? = null
+            //채팅방에 있는 유저 모두 체크
+            for (user in chatModel[position].users.keys) {
+                if (!user.equals(uid)) {
+                    destinationUid = user
+                    destinationUsers.add(destinationUid)
+                }
+            }
+            fireDatabase.child("users").child("$destinationUid").addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val friend = snapshot.getValue<Friend>()
+                    Glide.with(holder.itemView.context).load(friend?.profileImageUrl)
+                        .apply(RequestOptions().circleCrop())
+                        .into(holder.imageView)
+                    holder.textView_title.text = friend?.name
+                }
+            })
+            //메세지 내림차순 정렬 후 마지막 메세지의 키값을 가져
+            val commentMap = TreeMap<String, ChatFragment.ChatModel.Comment>(reverseOrder())
+            commentMap.putAll(chatModel[position].comments)
+            val lastMessageKey = commentMap.keys.toTypedArray()[0]
+            holder.textView_lastMessage.text = chatModel[position].comments[lastMessageKey]?.message
+
+            //채팅창 선책 시 이동
+            holder.itemView.setOnClickListener {
+                val intent = Intent(this@ChatListActivity, ChattingActivity::class.java)
+                intent.putExtra("destinationUid", destinationUsers[position])
+                startActivity(intent)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return chatModel.size
+        }
+    }
+
 }

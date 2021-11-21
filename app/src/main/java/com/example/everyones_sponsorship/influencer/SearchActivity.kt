73612cfe71ce46d1b57930576bbc6010d6cef217
sperospera1225@gmail.com <sperospera1225@gmail.com>
2021-11-22 02:29:11 +0900

    minseon

--- app/src/main/java/com/example/everyones_sponsorship/influencer/SearchActivity.kt
+++ app/src/main/java/com/example/everyones_sponsorship/influencer/SearchActivity.kt
@@ -3,14 +3,14 @@ package com.example.everyones_sponsorship.influencer
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.everyones_sponsorship.databinding.ActivitySearchBarBinding
+import com.example.everyones_sponsorship.databinding.ActivitySearchBinding
 import com.example.everyones_sponsorship.databinding.ActivitySearchresultBinding
 
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySearchBarBinding.inflate(layoutInflater)
+        val binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.imageView.setOnClickListener {
